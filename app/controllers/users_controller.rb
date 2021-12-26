1e978f822315652f08dd7db2cf2b412f372debb1
spookyorange <korkmazcoltu@mail.ru>
2021-12-26 16:38:39 +0300

    project completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
   def new
     @user = User.new
   end
